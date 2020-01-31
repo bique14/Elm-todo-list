d2c384add2e8d85f90be50b8163c8fc178209caa
bbibbibiq <peerasorn@hot-now.com>
2020-02-01 00:53:47 +0700

    Can add text to list

--- src/Main.elm
+++ src/Main.elm
@@ -1,47 +1,81 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
-type alias Model =
-    { count : Int }
-
-
-initialModel : Model
-initialModel =
-    { count = 0 }
+port sendTodo : String -> Cmd msg
 
 
 type Msg
-    = Increment
-    | Decrement
+    = NoOp
+    | Add
+    | UpdateText String
 
 
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Increment ->
-            { model | count = model.count + 1 }
-
-        Decrement ->
-            { model | count = model.count - 1 }
+type alias Model =
+    { text : String
+    , list : List String
+    , state : State
+    }
 
 
-view : Model -> Html Msg
-view model =
-    div []
-        [ button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
-        ]
+type State
+    = Init
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
+    Browser.element
+        { init = init
         , view = view
         , update = update
+        , subscriptions = \_ -> Sub.none
         }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { text = "", list = [], state = Init }, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case ( model.state, msg ) of
+        ( _, UpdateText s ) ->
+            ( { model | text = s }, Cmd.none )
+
+        ( _, Add ) ->
+            ( { model | list = model.list ++ [ model.text ] }, sendTodo model.text )
+
+        _ ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "m-2" ]
+        [ input
+            [ class "border border-black rounded px-2 py-1 w-8/12"
+            , placeholder "type"
+            , onInput UpdateText
+            ]
+            []
+        , button [ class "bg-blue-500 text-white rounded px-2 py-1 mx-2 w-20", onClick Add ] [ text "add" ]
+        , viewTodoList model.list
+        ]
+
+
+viewTodoList : List String -> Html Msg
+viewTodoList ls =
+    div []
+        (List.map
+            (\todo ->
+                div []
+                    [ text todo
+                    ]
+            )
+            ls
+        )
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { Elm } from './Main.elm'
 
 var app = Elm.Main.init({ node: document.querySelector('main') })
+
+app.ports.sendTodo.subscribe(function(todo) {
+  console.log(`todo from js ${todo}`)
+});
